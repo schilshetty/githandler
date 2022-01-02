5ea59000b7ed31d8d248fd6083de540ed713553e
schilshetty <schilshetty@deqode.com>
2022-01-02 19:17:51 +0530

    Project setup
    setup an express server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+
+
+init();
+
+async function init() {
+  try {
+    app.listen(3000, () => {
+      console.log(`Express App Listening on Port 3000!`);
+    });
+  } catch (error) {
+    console.error(`An error occurred: ${JSON.stringify(error)}`);
+    process.exit(1);
+  }
+}
