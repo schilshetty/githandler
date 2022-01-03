9781c6b4deae05fdb382965a84907dffdef95f56
schilshetty <schilshetty@deqode.com>
2022-01-03 05:59:58 +0530

    controllers and services setup
    setup the controllers and services for getting the repo details of the user

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const routes = require('./routes/github.routes')
 
 const sequelize = require("./database/models");
+const not_found = require("./middleware/route.not.found");
+const error_handler_middleware = require("./middleware/error.handler");
 
 
 sequelize.authenticate().then(() => console.log('connected to the database successfully'))
@@ -10,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/', routes)
+app.use('/api/v1', routes)
 
 app.use(not_found)
 app.use(error_handler_middleware)
